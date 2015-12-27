2e28dce7afdecc134e9c93269a9d55e7d75e7870
Steve Davis <steve@signafire.com>
2015-12-26 21:20:44 -0500

    fix erroneous scss glob path

--- dev/gulp/tasks/serve.js
+++ dev/gulp/tasks/serve.js
@@ -23,7 +23,7 @@ gulp.task('serve', function () {
 
   gulp.watch([global.paths.nunjucks], ['compile-nunjucks']).on('change', logChanges);
   gulp.watch([global.paths.html], ['reload:html']).on('change', logChanges);
-  gulp.watch([global.paths.scss], ['lint:scss', 'compile-scss:development']).on('change', logChanges);
+  gulp.watch([global.paths.scss.compile], ['lint:scss', 'compile-scss:development']).on('change', logChanges);
   gulp.watch([global.paths.js], ['lint:js', 'reload:js']).on('change', logChanges);
   gulp.watch([global.paths.e2e_tests], ['protractor']).on('change', logChanges);
 });
