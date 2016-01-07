c4ee552110cd579be8cd685b17688b1c108b06e4
Steve Davis <steve@signafire.com>
2016-01-07 17:01:26 -0500

    don't use ng-annotate since it doesn't parse ES6 correctly (because internally it uses a hard-coded config for the acorn javascript parser)

--- dev/gulp/tasks/optimize.js
+++ dev/gulp/tasks/optimize.js
@@ -1,24 +1,23 @@
 /**
  * Optimize assets (js):
- *  - ng-annotate
  *  - uglify
  **/
 
 var gulp = require('gulp');
 var gulpIf = require('gulp-if');
-var gulpNgAnnotate = require('gulp-ng-annotate');
 var gulpUglify = require('gulp-uglify');
 
 
+// Optimize the production build
 gulp.task('optimize', function () {
 
   return gulp.src(global.paths.dist + '/**')
 
-    //.pipe(gulpIf('*.js', gulpNgAnnotate()))
-
     // In addition to minifying via the jspm bundler, uglifyjs provides
     // some additional capabilities such as removing unreachable code, etc.
-    //.pipe(gulpIf('*.js', gulpUglify()))
+    .pipe(gulpIf('*.js', gulpUglify({
+      //mangle: false
+    })))
 
     .pipe(gulp.dest(global.paths.dist));
 });
