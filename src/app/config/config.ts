f5b835174ee6b0b91bb9e18b89d724d4d92f7c75
Steve Davis <steve@signafire.com>
2016-05-02 09:16:52 -0400

    chore(config): update REST API port config

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -3,8 +3,8 @@ import * as angular from 'angular';
 let configModule = angular
   .module('sf.config.api', [])
   .constant('API_CONFIG', {
-    authPort: '8092',
-    dataPort: '8092'
+    authPort: '8090',
+    dataPort: '8090'
   })
   .constant('PATH_CONFIG', {
     iconic: 'build/assets/img/iconic'
